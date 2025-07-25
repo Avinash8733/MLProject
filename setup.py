98adf9d4d3329f5e64d081083e11e120dba75fee
Avinash8733 <parmaravinash1122@gmail.com>
2025-07-25 09:11:24 +0530

    EDA and problem statement

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from typing import List
 
 HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
-    ''' 
-       this function will return the list of requirements
+    '''
+    this function will return the list of requirements
     '''
     requirements=[]
     with open(file_path) as file_obj:
@@ -13,14 +13,15 @@ def get_requirements(file_path:str)->List[str]:
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
+    
     return requirements
 
 setup(
-    name='mlproject',
-    version='0.0.1',
-    author='Avinash',
-    author_email='parmaravinash1122@gmail.com',
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
-    
-    )
+name='mlproject',
+version='0.0.1',
+author='Krish',
+author_email='krishnaik06@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
